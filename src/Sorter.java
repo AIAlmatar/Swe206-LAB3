7a8582f1a8917540b1aff8e9c46f9310af1dc898
Ali Almatar <ali@Alis-MacBook-Pro.local>
2024-02-13 19:55:33 +0300

    resolved conflicts

--- src/Sorter.java
+++ src/Sorter.java
@@ -1,23 +1,22 @@
 public class Sorter {
     public static void insertionSort(int array[]) {
-        int n = array.length;  
-        for (int j = 1; j < n; j++) {  
-            int key = array[j];  
-            int i = j-1;  
-            while ( (i > -1) && ( array [i] > key ) ) {  
-                array [i+1] = array [i];  
-                i--;  
-            }  
-            array[i+1] = key;  
+        int n = array.length;
+        for (int j = 1; j < n; j++) {
+            int key = array[j];
+            int i = j - 1;
+            while ((i > -1) && (array[i] > key)) {
+                array[i + 1] = array[i];
+                i--;
+            }
+            array[i + 1] = key;
         }
     }
-    
-    public static void selectionSort(int[] arr){
-        for (int i = 0; i < arr.length - 1; i++)
-        {
+
+    public static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
             int index = i;
-            for (int j = i + 1; j < arr.length; j++){
-                if (arr[j] < arr[index]){
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[index]) {
                     index = j;//searching for lowest index
                 }
             }
@@ -26,7 +25,14 @@ public class Sorter {
             arr[i] = smallerNumber;
         }
     }
-    public static void sort(int[] arr){
-        insertionSort(arr);
+
+    public static void sort(int[] arr, int sel) {
+        if (sel == 0) {
+            insertionSort(arr);
+        } else {
+            selectionSort(arr);
+        }
+
+
     }
 }
